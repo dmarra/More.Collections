c6492dc8051b7d0bf97c3e619c50e9eee1d98b15
dmarra <dan.marra@gmail.com>
2016-11-03 16:00:46 -0400

    Adds CircularQueue

---
+++ More.Collections/CircularQueue.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace More.Collections {
+    /// <summary>
+    /// The CircularQueue is a form of queue where the items are cycled
+    /// in a circular fashion. The list has no true beginning or end. The beginning
+    /// is always defined as the current item, and the end is the previously
+    /// visted one. 
+    /// 
+    /// When items are Enqueue()'d, they will always be added 
+    ///  
+    /// This is implimented as a forward only linked list.
+    /// </summary>
+    public class CircularQueue<T> : IEnumerable<T>, ICollection, IEnumerable  {       
+        private CircularQueueNode<T> currentNode;
+        private CircularQueueNode<T> previousNode;
+        
+        private object syncRoot;
+
+        public class CircularQueueNode<T>{
+            public T Item { get; set; }
+            public CircularQueueNode<T> Next { get; set; }
+
+            public CircularQueueNode(T item) {                
+                this.Item = item;
+            }
+        }
+
+        #region ENUMERATION
+        [Serializable]
+        ///<summary>
+        ///Enumerates the elements of a CircularQueue
+        ///</summary>            
+        public struct Enumerator : IEnumerator<T>, IDisposable, IEnumerator {            
+            CircularQueueNode<T> sNode;
+            CircularQueueNode<T> cNode;
+
+            internal Enumerator(CircularQueue<T> queue) {                            
+                sNode = queue.currentNode;
+                cNode = null;
+            }
+
+            public T Current {
+                get { 
+                    try {
+                        if (cNode == null) {
+                            throw new InvalidOperationException();
+                        }
+                        return cNode.Item;
+                    } catch (IndexOutOfRangeException) {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }                        
+
+            object IEnumerator.Current {
+                get {
+                    try {
+                        return (object)Current;
+                    } catch {
+                        throw;
+                    }
+                }
+            }
+
+            public bool MoveNext() {
+                bool complete = false;
+                if (cNode == null) {
+                    cNode = sNode;
+                    // the following indicates a queue of one item
+                    complete = cNode.Next == sNode;
+
+                } else {
+                    cNode = cNode.Next;
+                    complete = cNode == sNode;
+                }
+                return !complete;
+            }
+
+            public void Reset() {                
+            }
+
+            void IDisposable.Dispose() {
+                Reset();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() {
+            return new CircularQueue<T>.Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() {
+            return GetEnumerator();
+        }
+        #endregion
+
+        #region PROPERTIES
+        /// <summary>
+        /// Returns number of elements are in the queue
+        /// </summary>
+        /// <returns>The number of elements currently in the container</returns>
+        public int Count { get; private set; }
+        
+        /// <summary>
+        /// Indicates whether access to the container is thread-safe. This is always false.
+        /// Make sure to lock the container (or container.SyncRoot) before use if used accorss threads
+        /// </summary>
+        /// <returns>false</returns>
+        public bool IsSynchronized {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets an object that can be used to synchronize access to the <see cref="T:System.Collections.ICollection" />.
+        /// </summary>
+		/// <returns>
+        /// An object that can be used to synchronize access to the <see cref="T:System.Collections.ICollection" />.          
+        /// </returns>		
+        public object SyncRoot {
+            get {
+                if (this.syncRoot == null) {
+					Interlocked.CompareExchange(ref this.syncRoot, new object(), null);
+				}
+				return this.syncRoot;
+            }
+        }
+
+        public T Current {
+            get {
+                return currentNode.Item;
+            }
+        }
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public CircularQueue() {
+            Count = 0;
+        }               
+
+        public CircularQueue(IEnumerable<T> source) : this() {
+            if (source == null) {
+                throw new ArgumentNullException();
+            }            
+
+            int size = 0;
+            if (source.GetType().IsArray) {
+                size = ((T[])source).Length;
+            } else if (source is ICollection || source is ICollection<T>) {
+                size = ((ICollection)source).Count;
+            }
+
+            if (size <= 0) {
+                throw new ArgumentOutOfRangeException("Passed IEnumerable has no elements");
+            }
+           
+            foreach (T item in source) {
+                Enqueue(item);
+            }
+        }
+        #endregion
+
+        public void Enqueue(T item) {
+            if (Count == 0) {
+                currentNode = new CircularQueueNode<T>(item);
+                currentNode.Next = currentNode;
+
+            } else if (Count == 1) {
+                previousNode = new CircularQueueNode<T>(item);
+                previousNode.Next = currentNode;
+                currentNode.Next = previousNode;
+
+            } else {
+                CircularQueueNode<T> qn = new CircularQueueNode<T>(item);
+                qn.Next = currentNode;
+                previousNode.Next = qn;
+                previousNode = qn;
+            }
+            Count++;
+        }
+
+        public T Dequeue() {
+            if (Count > 0) {
+                T retVal = currentNode.Item;
+                CircularQueueNode<T> buffer = currentNode;
+                currentNode = buffer.Next;
+                buffer = null;
+
+                if (previousNode != null) {
+                    previousNode.Next = currentNode;
+                }
+
+                Count--;
+                return retVal;
+            } else {
+                throw new System.InvalidOperationException("The current CircularQueue is empty.");
+            }
+        }
+
+        public T Next() {
+            T retVal = currentNode.Item;
+            currentNode = currentNode.Next;
+
+            return retVal;
+        }
+
+        public T Peek() {
+            return currentNode.Next.Item;
+        }
+
+       
+
+        public void CopyTo(T[] array, int index) {
+            if (array == null) {
+                throw new ArgumentNullException("Array is null");
+            } else if(index < 0 || index > array.Length) {
+                throw new ArgumentOutOfRangeException("Start array index is out of range");
+            } else if (Count > array.Length - index) {
+                throw new ArgumentException("Array index would overflow");
+            }
+
+            int i = 0;
+            foreach (T item in this) {
+                array[i] = item;
+                i++;
+            }            
+        }
+
+        public void CopyTo(Array array, int index) {
+            CopyTo(array, index);
+        } 
+    }
+}
---
+++ UnitTests/CircularQueueTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using More.Collections;
+
+
+namespace UnitTests {
+    // We do not use the exception value a lot in these tests; we only
+    // care they were thrown, so disable this warning
+    #pragma warning disable 168
+
+    [TestClass]
+    public class CircularQueueTest {
+        [TestMethod]
+        public void TestInitialization() {
+            int[] testList = new int[] {1,2,3,4,5};
+            CircularQueue<int> cq = new CircularQueue<int>(testList);
+            Assert.IsTrue(cq.Count == 5, "Count is incorrect");
+            Assert.IsTrue(cq.Current == 1, "Unexpected initial value. Expected 1, but value was " + cq.Current);
+        }
+
+        [TestMethod]
+        public void TestEnumeration() {
+            int[] testList = new int[] {1,2,3,4,5};
+            CircularQueue<int> cq = new CircularQueue<int>(testList);
+
+            // test foreach looping
+            int current = cq.Current;
+            int iterationCount = 1;
+            foreach(int item in cq) {
+                Assert.IsTrue(
+                    current == item, 
+                    "Unexpected value encountered during foreach; start at natural beginning. \n" +
+                    "local current: " + current + "\n" +
+                    "foreach item: " + item + "\n" +
+                    "iteration count: " + iterationCount);
+                current++;
+                iterationCount++;
+            }
+
+            // test with a new beginning
+            cq.Next();
+            Assert.IsTrue(cq.Current == 2, "Unexpected current value; expected 2");
+            bool first = true;
+            current = 0;
+            foreach (int item in cq) {
+                if (first) {
+                    Assert.IsTrue(item == 2, "Foreach did not start at expected item; started at " + item);
+                    first = false;
+                }
+                current = item;
+            }           
+            Assert.IsTrue(current == 1, "Unexpected current value after foreach iteration. current: " + current);
+        }
+
+        [TestMethod]
+        public void TestPeek() {
+            int[] testList = new int[] {1,2,3,4,5};
+            CircularQueue<int> cq = new CircularQueue<int>(testList);
+
+            Assert.IsTrue(cq.Peek() == 2, "Unexpected Peek value; expected 2");
+            cq.Next();
+            Assert.IsTrue(cq.Peek() == 3, "Unexpected Peek value; expected 3, but value was " + cq.Peek());
+        }
+
+
+        [TestMethod]
+        public void TestEnqueueDequeue() {
+            CircularQueue<int> cq = new CircularQueue<int>();
+
+            for (int i = 1; i <= 5; i++) {
+                cq.Enqueue(i);
+            }
+            Assert.IsTrue(cq.Current == 1, "Unexpected current value after full enqueuing; expected 1");
+
+            int item = cq.Dequeue();
+            Assert.IsTrue(cq.Count == 4, "Unexpected count after dequeue");
+            Assert.IsTrue(item == 1, "Dequeue() returned wrong item");
+            Assert.IsTrue(cq.Current == 2, "Current is not correct after a Dequeue(); item was not advanced");
+
+            for (int i = cq.Count; i > 0; i--) {
+                cq.Dequeue();
+            }
+            Assert.IsTrue(cq.Count == 0, "Queue is not empty as expected.");
+
+            bool caught = false;
+            try {
+                cq.Dequeue();               
+            } catch (InvalidOperationException ex) {
+                caught = true;
+            }
+            Assert.IsTrue(caught, "Expected exception was not throwns; Dequeue() allowed on 0 sized queue.");
+        }
+
+
+        [TestMethod]
+        public void TestGC() {
+            CircularQueue<int> cq = new CircularQueue<int>();
+
+            for (int i = 1; i <= 5; i++) {
+                cq.Enqueue(i);
+            }
+
+
+            WeakReference reference = null;
+            new Action(() => 
+            {
+                int item = cq.Dequeue();  
+                reference = new WeakReference(item, true);
+            })();
+
+            // Item should have gone out of scope about now, 
+            // so the garbage collector can clean it up
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Assert.IsNull(reference.Target);
+        }
+    }
+}
